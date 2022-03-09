8cc74ebb3b82d2597b6d688f6204ed92eead307f
Santo-G <santogulletta91@gmail.com>
2022-03-09 18:44:13 +0100

    fix: minor fixes

--- app/src/main/java/com/androidapp/myfootballcoach/detail/DetailFragment.kt
+++ app/src/main/java/com/androidapp/myfootballcoach/detail/DetailFragment.kt
@@ -6,18 +6,24 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.androidapp.myfootballcoach.R
+import com.androidapp.myfootballcoach.databinding.FragmentDetailBinding
+import com.androidapp.myfootballcoach.domain.Player
+import com.androidapp.myfootballcoach.utils.bindings.viewBinding
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
-
+    private val args: DetailFragmentArgs by navArgs()
+    private val binding by viewBinding (FragmentDetailBinding::bind)
+    lateinit var player: Player
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        player = args.player
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // TODO
     }
 
 }
